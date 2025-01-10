38e27c85f68676fe9eafb27a208d91994cad73da
Tanveer Najib <tanveernajib@gmail.com>
2025-01-10 18:17:48 +0000

    Remove unneeded fields

--- providers/chapter.go
+++ providers/chapter.go
@@ -4,6 +4,5 @@ type Chapter struct {
 	Title          string `json:"title"`
 	ID             string `json:"id"`
 	Provider       string `json:"provider"`
-	Chapter        string `json:"chapter"`
 	AbsoluteNumber int    `json:"absoluteNumber"`
 }
